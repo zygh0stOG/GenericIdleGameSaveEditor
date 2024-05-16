4bc5ce4c0317bd39edea5527dff155ae9627f3fe
zygh0stOG <zygh0stog@gmail.com>
2024-05-16 09:21:21 -0500

    Too may things to list

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Generic Idle Game Save Editor
 
-A Pen created on CodePen.io. Original URL: [https://codepen.io/zygh0stOG/pen/NWJKrMJ](https://codepen.io/zygh0stOG/pen/NWJKrMJ).
+More or less what it says, it will let you edit the save from any game that allows exporting to a base64 encoded and/or LZ compressed string.
 
-More or less what it says, it will let you edit the save from any game that allows exporting to a base64 encoded or LZ compressed string.
+It utilizes the [JSON Tree View](https://github.com/zygh0stOG/json-tree-view) JavaScript module to render JSON and allow easy editing. Tested on [Evolve](https://pmotschmann.github.io/Evolve/).
--- dist/script.js
+++ dist/script.js
@@ -1,19 +1,23 @@
-import jsonTreeView from "https://esm.sh/json-tree-view";
+//import jsonTreeView from "https://esm.sh/json-tree-view";
+import jsonTreeView from "https://esm.sh/gh/zygh0stOG/json-tree-view";
 //var JSONTreeView = require('json-tree-view');
 
 //var view = new jsonTreeView('example', {hello: 'world'});
 
-let view = new jsonTreeView('Root',null);
+let view = new jsonTreeView("Evolve", null);
 let save_data = null;
 let isLZString = true;
 
-var encode = document.getElementById('encode'),
-    decode = document.getElementById('decode'),
-    output = document.getElementById('output'),
-    outputshit = document.getElementById('outputshit'),
-    input = document.getElementById('input');
+var encode = document.getElementById("btn-encode"),
+  decode = document.getElementById("btn-decode"),
+  //testbtn = document.getElementById('btn-test'),
+  //output = document.getElementById('sg-output'),
+  //outputStuff = document.getElementById('container-output'),
+  treeViewDiv = document.getElementById("tree-view"),
+  input = document.getElementById("sg-input");
 
-outputshit.appendChild(view.dom);
+//outputStuff.appendChild(view.dom);
+treeViewDiv.appendChild(view.dom);
 window.view = view;
 
 function get_value_from_object(...keys) {
@@ -40,40 +44,111 @@ function add_to_save(value, ...keys) {
   current[keys[keys.length - 1]] = parseFloat(value);
 }
 
-encode.addEventListener('click',encode_save);
+//testbtn.addEventListener('click', readTextFromClipboard);
+
+encode.addEventListener("click", encode_save);
 //encode.onclick = encode_save();
-  /*function() {
-    //output.innerHTML = LZString.compress(input.value);
-    input.value = LZString.compressToBase64(output.value);
+/*function() {
+  //output.innerHTML = LZString.compress(input.value);
+  input.value = LZString.compressToBase64(output.value);
 }*/
-    
-decode.addEventListener('click', decode_save);
+
+decode.addEventListener("click", decode_save);
 //decode.onclick = decode_save();
-  /*function() {
-    //var $str = output.innerHTML;
-    //output.innerHTML = LZString.decompress($str); 
-    output.value = LZString.decompressFromBase64(input.value);
-  input.value = "";
-    //output.innerHTML = LZString.decompressFromBase64(input.value); 
-    
+/*function() {
+  //var $str = output.innerHTML;
+  //output.innerHTML = LZString.decompress($str); 
+  output.value = LZString.decompressFromBase64(input.value);
+input.value = "";
+  //output.innerHTML = LZString.decompressFromBase64(input.value); 
+  
 }    
 */
+
+//const bs_arr = [];
+
+/*
+function readTextFromClipboard() {
+  let bs_str = null;
+  //const bs_str;
+  navigator.clipboard.readText()
+    .then((clipText) => (input.value = clipText)
+  );
+  doSomething();
+};
+*/
+
+function readTextFromClipboard() {
+  //  var bs_str =
+  readTextFromClipboard2();
+  //window.alert(bsObj.shit);
+  console.log(bsObj.shit);
+}
+
+var bsObj = { shit: "whatever" };
+
+const readTextFromClipboard2 = () => {
+  //var bsObj = { shit: "whatever" };
+  //bsObj.shit = "fuck";
+  navigator.clipboard
+    .readText()
+    .then(
+      (text) => {
+        doSomething(text);
+      } //{
+      //console.log('Clipboard content: ', text);
+      //return text;
+      //}
+    )
+    //.then()
+    .catch((err) => {
+      console.error("Failed to read clipboard contents: ", err);
+    });
+  //return bsObj.shit;
+};
+
+function doSomething(shit) {
+  //console.log(shit);
+  bsObj.shit = shit;
+  //console.log(bsObj.shit);
+  //return bs_str;
+  input.value = bsObj.shit;
+  return; // bsObj.shit;
+}
+
 function decode_save() {
+  var raw_str;
   try {
-    var raw_str = LZString.decompressFromBase64(input.value);
+    if (input.value === "") {
+      navigator.clipboard
+        .readText()
+        .then((clipText) => (input.value = clipText));
+      decode_save();
+      //input.value = raw_str;
+      //raw_str = LZString.decompressFromBase64(clipText)));
+      //raw_str = LZString.decompressFromBase64(clipText);
+      //input.value = clipText;
+    } else {
+      raw_str = LZString.decompressFromBase64(input.value);
+    }
     //input.value = "";
     save_data = JSON.parse(raw_str);
-  } catch (error) { 
-    var raw_str = atob(input.value);
-    save_data = JSON.parse(raw_str);
-    isLZString = false;
+  } catch (error) {
+    try {
+      var raw_str = atob(input.value);
+      save_data = JSON.parse(raw_str);
+      isLZString = false;
+    } catch (error) {
+      return;
+    }
   }
-  Cookies.set('savedata', input.value);
+  Cookies.set("savedata", input.value);
   //output.value = raw_str;
   //view = new jsonTreeView('Root',save_data);
   view.refresh();
   view.withRootName = false;
   view.showCountOfObjectOrArray = true;
+  view.includeTypeInCount = false;
   view.value = save_data;
   //console.log(view);
   //outputshit.appendChild(view.dom);
@@ -91,9 +166,10 @@ function encode_save() {
     enc_str = b64_str;
   }
   input.value = enc_str;
-  Cookies.set('savedata', enc_str);
+  Cookies.set("savedata", enc_str);
 }
-let tmp_str = Cookies.get('savedata');
-if (input.value === '' && !tmp_str === 'undefined') {
+
+let tmp_str = Cookies.get("savedata");
+if (input.value === "" && !tmp_str === "undefined") {
   input.value = tmp_str;
 }
